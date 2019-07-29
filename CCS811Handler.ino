40e647adcadc60f041045d41885cdeee9a36cc28
Matt Dresser <m.dres344@gmail.com>
2019-07-28 19:59:33 -0700

    Sustain core up and running with InfluxDB. Added handling for CCS811 gas sensor.

---
+++ CCS811Handler.ino
@@ -0,0 +1,38 @@
+#include <Wire.h>
+
+#include "SparkFunCCS811.h" //Click here to get the library: http://librarymanager/All#SparkFun_CCS811
+
+//#define CCS811_ADDR 0x5B //Default I2C Address
+#define CCS811_ADDR 0x5A //Alternate I2C Address
+
+CCS811 mySensor(CCS811_ADDR);
+
+void initCCS811()
+{
+  Serial.println("CCS811 Initializing");
+
+  Wire.begin(); //Inialize I2C Harware
+
+  //It is recommended to check return status on .begin(), but it is not
+  //required.
+  CCS811Core::status returnCode = mySensor.begin();
+  if (returnCode != CCS811Core::SENSOR_SUCCESS)
+  {
+    Serial.println(".begin() returned with an error.");
+    while (1); //Hang if there was a problem.
+  }
+}
+
+void pollCCS811(ccs811Sensor_t * sensor)
+{
+  //Check to see if data is ready with .dataAvailable()
+  if (mySensor.dataAvailable())
+  {
+    //If so, have the sensor read and calculate the results.
+    //Get them later
+    mySensor.readAlgorithmResults();
+
+    sensor->eco2 = mySensor.getCO2();
+    sensor->tvoc = mySensor.getTVOC();
+  }
+}
--- SustainCore.ino
+++ SustainCore.ino
@@ -12,7 +12,15 @@ typedef struct {
   float humidity;
 }dhtSensor_t;
 
-dhtSensor_t sensor;
+typedef struct {
+  float eco2;
+  float tvoc;
+}ccs811Sensor_t;
+
+
+dhtSensor_t dhtSensor;
+ccs811Sensor_t gasSensor;
+
 
 void setup() {
   Serial.begin(115200);
@@ -20,22 +28,29 @@ void setup() {
 
   initEspRestmitter();
   initDHT();
+  initCCS811();
   
   //ESP.deepSleep(10e6);
 }
 
 void loop() {
   
-  uint8_t result = readDHT(&sensor);
+  uint8_t result = readDHT(&dhtSensor);
+  pollCCS811(&gasSensor);
   // Check to see if it failed to read, if it did, cancel the loop 
   if (result) {
     delay(500);
     return;  
   }
 
-  //result = espPost(sensor.temperature, sensor.humidity);
-  result = espDynamicPost("temperature", sensor.temperature);
-  result = result & espDynamicPost("humidity", sensor.humidity);
+  //result = espPost(sensor.temperature, dhtSensor.humidity);
+  result = espDynamicPost("temperature", dhtSensor.temperature);
+  result = result & espDynamicPost("humidity", dhtSensor.humidity);
+  if ((gasSensor.eco2 > 0.0) || (gasSensor.tvoc > 0.0))
+  {
+    result = result & espDynamicPost("eco2", gasSensor.eco2);
+    result = result & espDynamicPost("tvoc", gasSensor.tvoc);  
+  }
 
   // Wasn't able to post something, retry
   if (result) {
